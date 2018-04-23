0c0e98be9eb3f467602979a02db03f02eaddbd24
Tupt <tu@agiletech.vn>
2018-04-23 15:40:17 +0700

    added install guide

--- chaincode/loyalty/startChaincode.sh
+++ chaincode/loyalty/startChaincode.sh
@@ -3,4 +3,4 @@ if [[ ! -z $chaincodeID ]];then
   kill $chaincodeID
 fi
 # sleep 1
-go build -i && CORE_PEER_ADDRESS=localhost:7052 CORE_PEER_LOCALMSPID=Org1MSP CORE_VM_ENDPOINT=unix:///var/run/docker.sock CORE_CHAINCODE_ID_NAME=mycc:1 ./loyalty
+go build -i && CORE_PEER_ADDRESS=localhost:7052 CORE_PEER_LOCALMSPID=Org1MSP CORE_VM_ENDPOINT=unix:///var/run/docker.sock CORE_CHAINCODE_ID_NAME=mycc:1.0 ./loyalty
